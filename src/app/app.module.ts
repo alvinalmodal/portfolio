34d8934474d487ffb0128bd829033c603b80e907
alvin almodal <almodalalvin@gmail.com>
2020-08-13 13:57:59 +0800

    added header, about and projects page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule
